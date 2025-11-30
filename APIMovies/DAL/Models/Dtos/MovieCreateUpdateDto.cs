47fa3e322abdc9674d181d2cbd02f201a649b866
Christian <Christian04copetef@gmail.com>
2025-11-29 22:34:57 -0500

    Se crearon los Dto's de la entidad Movie y tambien se agrego al automapper

---
+++ APIMovies/DAL/Models/Dtos/MovieCreateUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIMovies.DAL.Models.Dtos
+{
+    public class MovieCreateUpdateDto
+    {
+        [Required(ErrorMessage = "El nombre de la categoria es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El número máximo de caracteres es de 100")]
+        public string Name { get; set; }
+        [Required]
+        public int Duration { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        [MaxLength(10, ErrorMessage = "El número máximo de caracteres es de 10")]
+        public string Clasification { get; set; }
+    }
+}
